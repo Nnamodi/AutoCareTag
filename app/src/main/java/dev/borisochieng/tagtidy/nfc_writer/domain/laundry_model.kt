ba8c533a78aba28191e1b319056f70e20167ef7d
toby1907 <olaleyetobi3@gmail.com>
2024-07-27 12:36:06 +0100

    implemented the writer screen

--- app/src/main/java/dev/borisochieng/tagtidy/nfc_writer/domain/laundry_model.kt
+++ app/src/main/java/dev/borisochieng/tagtidy/nfc_writer/domain/laundry_model.kt
@@ -5,6 +5,7 @@ package dev.borisochieng.tagtidy.nfc_writer.domain
 data class LaundryInfo(
     val clothType: String,
     val customerName: String,
+    val customerPhoneNo: String,
     val washingInstructions: String,
     val customerAddress: String,
     val amountToBePaid: String,
